a433084e4e3e1a65ab2203e6227aacbb386a1c1e
victruong <victortruongdo@gmail.com>
2019-12-22 18:40:08 -0500

    Sign In Page.

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import logo from './logo.svg';
-import ListTest from './ListTest.js'
+import ListTest from './ListTest.js';
 import './App.css';
+import LogInForm from './components/LogIn';
+import Login from './pages/sign_in';
 
 function App() {
     return (
@@ -21,6 +23,7 @@ function App() {
                     </a>
                 </header>
             <ListTest />
+        <Login />
         </div>
     );
 }
